ca41846b3c987757381d2ea44a852bb842b68dab
skizzo112 <64227864+skizzo112@users.noreply.github.com>
2021-07-27 12:08:36 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+// ELEMENT SELECT FUNCTION
+const selectElement = function (element) {
+    return document.querySelector(element)
+}
+
+let menuToggle = selectElement (".menu-toggle");
+let body = selectElement ("body");
+
+menuToggle.addEventListener("click", function() {
+    body.classList.toggle("open");
+});
+
+
+// // SCROLL REVEAL
+// window.sr = ScrollReveal();
+
+// sr.reveal = (".animate-left", {
+//     origin: "left",
+//     duration: 1000,
+//     distance: "25rem",
+//     delay: 300
+// });
+
+// sr.reveal = (".animate-right", {
+//     origin: "right",
+//     duration: 1000,
+//     distance: "25rem",
+//     delay: 600
+// });
+
+// sr.reveal = (".animate-top", {
+//     origin: "top",
+//     duration: 1000,
+//     distance: "25rem",
+//     delay: 600
+// });
+
+// sr.reveal = (".animate-bottom", {
+//     origin: "bottom",
+//     duration: 1000,
+//     distance: "25rem",
+//     delay: 600
+// });
+
